22e19f9f6fb71c4be2b2e3df033ce100ae8cb94e
Benjamin Wassink <bwassink@gmail.com>
2023-08-28 17:04:31 -0500

    Added Fandoms page changed nav to reflect

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,4 +1,4 @@
 <header>
   <h1>Wassink.io</h1>
-  <nav><a href="/">Home</a> | <a href="about">About</a> | <a href="quiz">Quiz</a></nav>
+  <nav><a href="/">Home</a> | <a href="blog">Blog</a> | <a href="quiz">Quiz</a> | <a href="fandoms">Fandoms</a></nav>
 </header>
---
+++ src/routes/fandoms/+page.svelte
@@ -0,0 +1,3 @@
+<h2>Skol Vikes!</h2>
+<h2>Listos Verde!</h2>
+<h2>Go Totties!</h2>
