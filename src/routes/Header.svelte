c6df52de37a79adb341e0001469205407049ab40
Benjamin Wassink <bwassink@gmail.com>
2023-09-01 16:42:20 -0500

    Made some changes to help with mobile layout

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,4 +1,4 @@
 <header>
-  <h1>Wassink.io</h1>
+  <div class="title">Wassink.io</div>
   <nav><a href="/">Home</a> | <a href="blog">Blog</a> | <a href="quiz">Quiz</a> | <a href="fandoms">Fandoms</a></nav>
 </header>
