df3abafad5b8a15e711a0157056d4e21af47b815
Benjamin Wassink <bwassink@gmail.com>
2024-03-03 13:55:50 -0600

    Remove JS30 from menu on site

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,4 +1,4 @@
 <header>
   <div class="title"><a href="/">Wassink.io</a></div>
-  <nav><a href="/">Home</a> | <a href="blog">Blog</a> | <a href="quiz">Quiz</a> | <a href="fandoms">Fandoms</a> | <a href="js30">JS30</a></nav>
+  <nav><a href="/">Home</a> | <a href="blog">Blog</a> | <a href="quiz">Quiz</a> | <a href="fandoms">Fandoms</a> <!--| <a href="js30">JS30</a>--></nav>
 </header>
