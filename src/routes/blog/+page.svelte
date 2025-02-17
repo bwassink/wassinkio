bb3bae3bef735b15cf531fd06e4bc527d9d3fbfd
Benjamin Wassink <bwassink@gmail.com>
2025-02-17 09:32:12 -0600

    Set import  and importing blog content to blog page.

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,2 +1,26 @@
+<script>
+  import { onMount } from 'svelte';
+  import sanity from '@sanity/client';
+  import sanityConfig from '../wassinksanity/sanity.config';
+
+  const client = sanity(sanityConfig);
+
+  let posts = [];
+
+  onMount(async () => {
+    const response = await client.fetch('*[_type == "post"]');
+    posts = response;
+  });
+</script>
+
 <h1>Hi, how are ya?</h1>
 <p>Is this thing on? 🎤</p>
+
+<ul>
+  {#each posts as post}
+    <li>
+      <h2>{post.title}</h2>
+      <p>Category: {post.category}</p>
+    </li>
+  {/each}
+</ul>
--- wassinksanity/schemaTypes/index.ts
+++ wassinksanity/schemaTypes/index.ts
@@ -1,3 +1,12 @@
+import sanity from '@sanity/client'
+import sanityConfig from './wassinksanity/sanity.config'
+
+const client = sanity(sanityConfig)
+
+// Use the client to fetch data from Sanity
+client.fetch('*[_type == "post"]').then((posts) => {
+  console.log(posts)
+})
 import blockContent from './blockContent'
 import category from './category'
 import post from './post'
